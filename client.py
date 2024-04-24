adb21bb93b5d6064cf7fbf73462050ffa73304cd
romanryzh09 <romkinapochta2017@mail.ru>
2024-04-24 21:13:50 +0300

    Импортирование игрового модуля pygame, создание окна с помощью pygame. Создание игрового цикла с принятием информации от сервера.

--- client.py
+++ client.py
@@ -1,8 +1,35 @@
 import socket
+import pygame
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 sock.connect(('localhost', 10000))
 
-while True:
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Бактерии')
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            run = False
     sock.send('Привет'.encode())
+
+    data = sock.recv(1024).decode()
+    print('Получил:', data)
+
+
+
+
+
+
+
+
+
+
+pygame.quit()
