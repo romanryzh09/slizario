44d3d1a715ee8faff0c301eb8130be9c818fd5b6
romanryzh09 <romkinapochta2017@mail.ru>
2024-04-24 20:49:09 +0300

    Создали бесконечный цикл, который позволяет безостановочно производить отправку данных на сервер.

--- client.py
+++ client.py
@@ -3,3 +3,6 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 sock.connect(('localhost', 10000))
+
+while True:
+    sock.send('Привет'.encode())
